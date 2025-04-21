2eb5a488ff0941ca945cdd8f69a37732ebb7beb7
Etele03 <etele.bugattiveyron@gmail.com>
2025-04-21 12:49:12 +0200

    Áruház hozzáadása elméletileg működik

--- Back-End/connection_test.php
+++ Back-End/connection_test.php
@@ -1,13 +1,13 @@
 <?php 
     include "connect.php";
 
-    $query="SELECT * FROM FELHASZNALO";
+    $query="SELECT * FROM ARUHAZ";
     $stid = oci_parse($conn, $query);  // A lekérdezés előkészítése
     oci_execute($stid);  // A lekérdezés végrehajtása
 
     // Eredmények kiíratása
     while ($row = oci_fetch_assoc($stid)) {
-        echo $row['FELHASZNALONEV'] . "<br>";  // Az eredményeket a megfelelő oszlopokkal kiírni
+        echo $row['CIM'] . "<br>";  // Az eredményeket a megfelelő oszlopokkal kiírni
     }
 
     // Kapcsolat bezárása
