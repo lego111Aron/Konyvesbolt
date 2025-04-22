878b78c8878a23b6fdf8bdda6a484b0acde4c0b6
Etele03 <etele.bugattiveyron@gmail.com>
2025-04-22 12:16:41 +0200

    alműfaj hozzáadása működik

--- Back-end/shop-genre/shop-genre-test.php
+++ Back-end/shop-genre/shop-genre-test.php
@@ -1,7 +1,7 @@
 <?php
 include "../connect.php";
 
-$query = "SELECT MUFAJ_NEV FROM MUFAJ";
+$query = "SELECT ALMUFAJ_NEV FROM ALMUFAJ";
 $stid = oci_parse($conn, $query);
 oci_execute($stid);
 
@@ -9,7 +9,7 @@ echo "<h2>Műfajok listája:</h2>";
 echo "<ul>";
 
 while ($row = oci_fetch_assoc($stid)) {
-    echo "<li>" . htmlspecialchars($row['MUFAJ_NEV']) . "</li>";
+    echo "<li>" . htmlspecialchars($row['ALMUFAJ_NEV']) . "</li>";
 }
 
 echo "</ul>";
