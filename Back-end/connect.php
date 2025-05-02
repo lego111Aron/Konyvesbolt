06be89182b992a2e881380773c6df6c8b6993fa0
Palfi Aron <h376388@stud.u-szeged.hu>
2025-05-02 21:24:16 +0200

    quickfix

--- Back-end/connect.php
+++ Back-end/connect.php
@@ -9,9 +9,7 @@ $tns = "
     )
 )";
 
-// FIXME: vissza kell állítani az eredetire
-// $conn = oci_connect('C##O4005C', 'Adatbalapu123', $tns, 'UTF8');
-$conn = oci_connect('C##JCNOSO', '34757268tIsH', $tns, 'UTF8');
+$conn = oci_connect('C##O4005C', 'Adatbalapu123', $tns, 'UTF8');
 
 if (!$conn) {
     $error_message = oci_error();
